7091aa9ea3be35b9539261cbc00c4a95c559fc33
Inês Soares <76999213+inesosoares6@users.noreply.github.com>
2021-03-10 18:06:15 +0000

    Add files via upload

--- src/listener_HL.cpp
+++ src/listener_HL.cpp
@@ -11,54 +11,32 @@
 using namespace message_filters;
 using namespace geometry_msgs;
 
-std::ofstream myfile;
-
  void callback(const geometry_msgs::PoseStamped::ConstPtr& hololens, const geometry_msgs::PoseStamped::ConstPtr& opti)
 {
-    float error_x, error_y, error_z, error, error_quad;
+    float error;
     float dist_hololens, dist_optiTrack;
-    error_x = hololens->pose.position.x - opti->pose.position.x;
-    error_y = hololens->pose.position.y - opti->pose.position.y;
-    error_z = hololens->pose.position.z - opti->pose.position.z;
     dist_hololens = sqrt(pow(hololens->pose.position.x,2)+pow(hololens->pose.position.y,2)+pow(hololens->pose.position.z,2));
     dist_optiTrack = sqrt(pow(opti->pose.position.x,2)+pow(opti->pose.position.y,2)+pow(opti->pose.position.z,2));
-    error_quad = pow(dist_hololens-dist_optiTrack,2);
     error = dist_hololens-dist_optiTrack;
 
-    myfile << hololens->header.stamp.sec << "." << hololens->header.stamp.nsec << ","  << error_x << "," << error_y << "," << error_z << "," << error << "," << error_quad << "\n";
     ROS_INFO("Error: [%f]", error);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "sync_HL_OT");
+  ros::init(argc, argv, "listener_HL");
 
-  ros::NodeHandle nh_param("~");
   ros::NodeHandle nh;
-
-  std::string testNumber;
-  std::string conditions;
-  nh_param.getParam("testNumber", testNumber);
-  ROS_INFO("Test Number : %s", testNumber.c_str());
 
   message_filters::Subscriber<PoseStamped> hololens_sub(nh, "HLposition", 1);
   message_filters::Subscriber<PoseStamped> optiTrack_sub(nh, "vrpn_client_node/IndexFinger/pose", 1);
 
   typedef sync_policies::ApproximateTime<PoseStamped, PoseStamped> MySyncPolicy;
 
-  std::string fileName;
-  std::string nameDevice;
-  nameDevice = "HL_OT_data";
-  fileName = nameDevice + testNumber.c_str();
-  myfile.open(fileName);
-  myfile << "Timestamp, Error x, Error y, Error z, Error, Quadratic Error\n";
-
   Synchronizer<MySyncPolicy> sync(MySyncPolicy(1000),hololens_sub, optiTrack_sub);
   sync.registerCallback(boost::bind(&callback, _1, _2));
 
   ros::spin();
-
-  myfile.close();
 
   return 0;
 }
--- src/listener_HTC.cpp
+++ src/listener_HTC.cpp
@@ -11,54 +11,32 @@
 using namespace message_filters;
 using namespace geometry_msgs;
 
-std::ofstream myfile;
-
  void callback(const geometry_msgs::PoseStamped::ConstPtr& htcvive, const geometry_msgs::PoseStamped::ConstPtr& opti)
 {
-    float error_x, error_y, error_z, error, error_quad;
+    float error;
     float dist_htcvive, dist_optiTrack;
-    error_x = htcvive->pose.position.x - opti->pose.position.x;
-    error_y = htcvive->pose.position.y - opti->pose.position.y;
-    error_z = htcvive->pose.position.z - opti->pose.position.z;
     dist_htcvive = sqrt(pow(htcvive->pose.position.x,2)+pow(htcvive->pose.position.y,2)+pow(htcvive->pose.position.z,2));
     dist_optiTrack = sqrt(pow(opti->pose.position.x,2)+pow(opti->pose.position.y,2)+pow(opti->pose.position.z,2));
-    error_quad = pow(dist_htcvive-dist_optiTrack,2);
     error = dist_htcvive-dist_optiTrack;
 
-    myfile << htcvive->header.stamp.sec << "." << htcvive->header.stamp.nsec << error_x << "," << error_y << "," << error_z << "," << error << "," << error_quad << "\n";
     ROS_INFO("Error: [%f]", error);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "sync_HTC_OT");
+  ros::init(argc, argv, "listener_HTC");
 
-  ros::NodeHandle nh_param("~");
   ros::NodeHandle nh;
-
-  std::string testNumber;
-  std::string conditions;
-  nh_param.getParam("testNumber", testNumber);
-  ROS_INFO("Test Number : %s", testNumber.c_str());
 
   message_filters::Subscriber<PoseStamped> htcvive_sub(nh, "HTCposition", 1);
   message_filters::Subscriber<PoseStamped> optiTrack_sub(nh, "vrpn_client_node/IndexFinger/pose", 1);
 
   typedef sync_policies::ApproximateTime<PoseStamped, PoseStamped> MySyncPolicy;
 
-  std::string fileName;
-  std::string nameDevice;
-  nameDevice = "HTC_OT_data";
-  fileName = nameDevice + testNumber.c_str();
-  myfile.open(fileName);
-  myfile << "Timestamp, Error x, Error y, Error z, Error, Quadratic Error\n";
-
-  Synchronizer<MySyncPolicy> sync(MySyncPolicy(1000),htcvive_sub, optiTrack_sub);
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(1000), htcvive_sub, optiTrack_sub);
   sync.registerCallback(boost::bind(&callback, _1, _2));
 
   ros::spin();
-
-  myfile.close();
 
   return 0;
 }
